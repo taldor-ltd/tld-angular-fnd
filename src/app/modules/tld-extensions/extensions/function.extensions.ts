70e882cf2f5e570854eb8dda1d671db7f9fcba51
Ofer Aharoni <ofera@Taldor.corp>
2020-02-18 10:45:06 +0200

    remove rest parameters

--- src/app/modules/tld-extensions/extensions/function.extensions.ts
+++ src/app/modules/tld-extensions/extensions/function.extensions.ts
@@ -1,31 +1,31 @@
 declare global {
     interface Function {
-        callDelayed(previousThis: any, ...args: any): any;
-        callDelayedTimout(previousThis: any, timeout: number, ...args: any): any;
+        // callDelayed(previousThis: any, ...args: any): any;
+        // callDelayedTimout(previousThis: any, timeout: number, ...args: any): any;
         applyDelayed(previousThis: any, args?: any[]): any;
         applyDelayedTimout(previousThis: any, timeout: number, args?: any[]): any;
     }
 }
-Function.prototype.callDelayed = function(previousThis: any, ...args: any) {
+// Function.prototype.callDelayed = function(previousThis: any, ...args: any) {
 
-    if (typeof this !== 'function') {
-        throw new Error(this + 'cannot be bound as it\'s not callable');
-    }
-    const boundTargetFunction = this;
-    setTimeout(() => {
-        boundTargetFunction.call(previousThis, ...args);
-    }, 0);
-};
+//     if (typeof this !== 'function') {
+//         throw new Error(this + 'cannot be bound as it\'s not callable');
+//     }
+//     const boundTargetFunction = this;
+//     setTimeout(() => {
+//         boundTargetFunction.call(previousThis, ...args);
+//     }, 0);
+// };
 
-Function.prototype.callDelayedTimout = function(previousThis: any, timeout: number, ...args: any) {
-    if (typeof this !== 'function') {
-        throw new Error(this + 'cannot be bound as it\'s not callable');
-    }
-    const boundTargetFunction = this;
-    setTimeout(() => {
-        boundTargetFunction.call(previousThis, ...args);
-    }, timeout);
-};
+// Function.prototype.callDelayedTimout = function(previousThis: any, timeout: number, ...args: any) {
+//     if (typeof this !== 'function') {
+//         throw new Error(this + 'cannot be bound as it\'s not callable');
+//     }
+//     const boundTargetFunction = this;
+//     setTimeout(() => {
+//         boundTargetFunction.call(previousThis, ...args);
+//     }, timeout);
+// };
 
 
 Function.prototype['applyDelayed'] = function(previousThis: any,  args?: any[]) {
